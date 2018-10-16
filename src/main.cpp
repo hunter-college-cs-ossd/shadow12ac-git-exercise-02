604eb4afdb5759438ca197d4a83011e941fda01a
Francis Irizarry <tuxwonder7@gmail.com>
2018-10-16 13:20:37 -0400

    Reverted to original design

--- src/main.cpp
+++ src/main.cpp
@@ -30,9 +30,7 @@
  #include <cstdlib>
  #include <cstdio>
  #include <cstring>
- #include <limits>
  #include <sstream>
- #include <stdlib.h>
 
   using namespace std;
  /******************************************************************************/
@@ -44,43 +42,24 @@ typedef ostream& (*output_function) ( ostream & out);
  ******************************************************************************/
 //typedef ostream& (*output_function) (const ostream & out);
 
+ostream& shadow12ac_oddity(ostream & out);
 
-
-//Get File String for specific Location
-fstream& StrFromLine(fstream& file, unsigned int Exactlocation){
-	for(unsigned int currentLoc = 0; currentLoc < Exactlocation-1; currentLoc++){
-		file.ignore(numeric_limits<streamsize>::max(), '\n');
-	}
-	return file;
-}
-
-ostream& shadow12ac_oddity(ostream & out){
-    return out;
-}
 
 
 int main(int argc, char* argv[] )
 {
-	if(argc < 2){
-		cerr << "Usage: ./executable textFile " << endl;
-		return 0;
-	}
 
-	stringstream strFile(argv[1]); //can also do char *file1 = argv[1]/stringstream(argv[1])
-	string fileName;
-	strFile >> fileName;
-	fstream odditiesFile(fileName.c_str());
-	srand (time(NULL));
-	StrFromLine(odditiesFile, (rand() % 15 + 1));
-	string oddityStr;
-	getline(odditiesFile, oddityStr);
-	output_function typeFunc;
-	typeFunc = shadow12ac_oddity;
-	typeFunc(cout << oddityStr << endl);
-    return 0;
+	shadow12ac_oddity(cout);
+        return 0;
 
 
 } 
+
+ostream& shadow12ac_oddity(ostream & out){
+    out << "Why do we drive on parkways and park on driveways?" << endl;
+	
+   return out;
+}
 
 /***********************************
 Original Design, Can be updated Easily. 
