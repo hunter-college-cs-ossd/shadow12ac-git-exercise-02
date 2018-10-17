01a10a132a05ee6f0e039ec5e5b567c19341159e
shadow12ac <Amal.mittal31@myhunter.cuny.edu>
2018-10-16 22:44:55 -0400

    starting oddity

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
   Title          : main.cpp
-  Author         : 
+  Author         : Shadow12ac
   Created on     : October  7, 2018
   Description    : Displays random peculiar facts
   Purpose        : To exercise collaborative source code development
@@ -43,7 +43,7 @@ typedef ostream& (*output_function) (const ostream & out);
                         All Function Definitions
 
 ******************************************************************************/
-
+ostream& starting_oddity( ostream & out);
 
 
 
@@ -53,8 +53,14 @@ int main(int argc, char* argv[] )
 {
 
     // Calls to output functions
-
+	starting_oddity(cout);
 
 
     return 0;
+}
+
+ostream& starting_oddity(ostream & out){
+	out << "This is a collection of strange but true facts." << endl;
+	
+	return out;
 }
